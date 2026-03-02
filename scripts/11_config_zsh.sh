1f1c7436399ff7ce8faacdcb4474baf763ad8cdf
claymore <jorgesqui92@yahoo.es>
2026-03-02 17:37:59 +0100

    zsh in root

--- scripts/11_config_zsh.sh
+++ scripts/11_config_zsh.sh
@@ -3,7 +3,16 @@ set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 SOURCE_ZSHRC="${ROOT_DIR}/configs/zshrc"
+SOURCE_ZSHRC_ROOT="${ROOT_DIR}/configs/zshrc_root"
 SOURCE_P10K="${ROOT_DIR}/configs/p10k.zsh"
+
+if [[ "${EUID}" -ne 0 ]]; then
+  SUDO="sudo"
+else
+  SUDO=""
+fi
 
 cp "${SOURCE_ZSHRC}" "${HOME}/.zshrc"
 cp "${SOURCE_P10K}" "${HOME}/.p10k.zsh"
+${SUDO} cp "${SOURCE_ZSHRC_ROOT}" /root/.zshrc
+${SUDO} cp "${SOURCE_P10K}" /root/.p10k.zsh
