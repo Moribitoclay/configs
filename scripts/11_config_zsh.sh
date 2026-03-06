1d2705f272e8082454fb7be07a3b16873f53ff47
claymore <jorgesqui92@yahoo.es>
2026-03-06 20:50:12 +0100

    install p10k

--- scripts/11_config_zsh.sh
+++ scripts/11_config_zsh.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 SOURCE_ZSHRC="${ROOT_DIR}/configs/zshrc"
-SOURCE_ZSHRC_ROOT="${ROOT_DIR}/configs/zshrc_root"
+SOURCE_ZSHRC_ROOT="${ROOT_DIR}/configs/zshrc"
 SOURCE_P10K="${ROOT_DIR}/configs/p10k.zsh"
 
 if [[ "${EUID}" -ne 0 ]]; then
@@ -16,3 +18,5 @@ cp "${SOURCE_ZSHRC}" "${HOME}/.zshrc"
 cp "${SOURCE_P10K}" "${HOME}/.p10k.zsh"
 ${SUDO} cp "${SOURCE_ZSHRC_ROOT}" /root/.zshrc
 ${SUDO} cp "${SOURCE_P10K}" /root/.p10k.zsh
+
+${SUDO} git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/powerlevel10k
